05e6ed961931fed23347165d18c1ca1e72338638
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2025-11-08 14:38:54 +0000

    Scaffold core module structure

---
+++ driftdetect.py
@@ -0,0 +1,12 @@
+# driftdetect.py
+"""
+Main module for DriftDetect application.
+"""
+
+import argparse
+import logging
+import os
+import sys
+from typing import Optional
+from dotenv import load_dotenv
+
